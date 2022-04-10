cead9faf7f222efec614be2f8b970683553501f1
Dusan Jovanov <jovanovdusan1@gmail.com>
2022-04-11 00:29:32 +0200

    updated example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,40 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { ActionArgs, createStore } from '../.';
+
+type State = {
+  count: number;
+};
+
+const { Provider, useModelState, useActions } = createStore({
+  models: {
+    count: 0,
+  } as State,
+  actions: {
+    increment: ({ set }: ActionArgs<State>) => {
+      set('count', s => s + 1);
+    },
+  },
+});
+
+const Counter = () => {
+  const count = useModelState('count');
+  const { increment } = useActions();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <Provider>
+      <Counter />;
+    </Provider>
   );
 };
 
